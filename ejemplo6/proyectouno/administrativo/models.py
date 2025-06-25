4dcb08a9b3fd4d17416ce8d19727fa1ad6f17148
Alex <alexis.rj2110@gmail.com>
2025-06-25 09:15:13 -0500

    V0.0.1

---
+++ ejemplo6/proyectouno/administrativo/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+
+# Create your models here.
+
+class Estudiante(models.Model):
+    opciones_tipo_estudiante = (
+        ('becado', 'Estudiante Becado'),
+        ('no-becado', 'Estudiante No Becado'),
+        )
+
+    nombre = models.CharField("Nombre de estudiante", max_length=30)
+    apellido = models.CharField(max_length=30)
+    cedula = models.CharField(max_length=30, unique=True)
+    edad = models.IntegerField("edad de estudiante") # Verbose field names
+    tipo_estudiante = models.CharField(max_length=30, \
+            choices=opciones_tipo_estudiante)
+    modulos = models.ManyToManyField('Modulo', through='Matricula')
+
+
+    def __str__(self):
+        return "%s - %s - %s - edad: %d - tipo: %s" % (self.nombre,
+                self.apellido,
+                self.cedula,
+                self.edad,
+                self.tipo_estudiante)
+
+    def obtener_matriculas(self):
+        return self.lasmatriculas.all()
+
+    def calcular_monto_total(self):
+        total = 0
+        for m in self.obtener_matriculas():
+            total += m.costo
+        return total
+
+class Modulo(models.Model):
+    """
+    """
+    opciones_modulo = (
+        ('1', 'Primero'),
+        ('2', 'Segundo'),
+        ('3', 'Tercero'),
+        ('4', 'Cuarto'),
+        ('5', 'Quinto'),
+        ('6', 'Sexto'),
+        )
+
+    nombre = models.CharField(max_length=30, \
+            choices=opciones_modulo)
+    estudiantes = models.ManyToManyField(Estudiante, through='Matricula')
+
+    def __str__(self):
+        return "Módulo: %s" % (self.nombre)
+
+
+class Matricula(models.Model):
+    """
+    """
+    estudiante = models.ForeignKey(Estudiante, related_name='lasmatriculas',
+            on_delete=models.CASCADE)
+    modulo = models.ForeignKey(Modulo, related_name='lasmatriculas',
+            on_delete=models.CASCADE)
+    costo = models.DecimalField(max_digits=10, decimal_places=2)
+    comentario = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "Matricula: Estudiante(%s) - Modulo(%s)" % \
+                (self.estudiante, self.modulo.nombre)
+
